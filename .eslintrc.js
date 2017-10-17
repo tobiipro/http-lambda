3a3ae0ce614758234541a0eaaec52912ea1e53df
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2017-10-17 12:37:05 +0200

    use firecloud eslint node config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   root: true,
   extends: [
-    'firecloud/configs/basic.js',
-    'firecloud/configs/no-null.js',
-    'firecloud/configs/max-len-2.js',
-    'firecloud/configs/node.js'
+    'firecloud/node.js'
   ]
 };
