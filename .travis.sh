e8dfefd714de6d0aa5917e4faf3c0bf6b1baa3d4
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-07 16:47:57 +0100

    0.1.1 (dist)

--- .travis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# travis_run_<step>() {
-# }
-
-SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/support-firecloud" && pwd)"
-source "${SUPPORT_FIRECLOUD_DIR}/repo/dot.travis.sh.sf"
