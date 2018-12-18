6118f874736bcb1bb65c6bc12e692d5873187253
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-18 17:50:14 +0100

    add test for requestContext

--- test/lambda-http.test.js
+++ test/lambda-http.test.js
@@ -48,4 +48,26 @@ describe('instance of LambdaHttp', function() {
       res.end();
     });
   });
+
+  it('makes ctx.requestContext equal to e.requestContext for convenience', function(done) {
+    let e = {
+      requestContext: {
+        foo: 'bar'
+      }
+    };
+    let ctx = {};
+
+    let cb = function(_err, _jsonRes) {
+      done();
+    };
+
+    let http = new LambdaHttp(e, ctx, cb);
+    http.createServer(function(req, res) {
+      // eslint-disable-next-line jest/prefer-strict-equal
+      expect(req.ctx.requestContext).toEqual(e.requestContext);
+      // eslint-disable-next-line jest/prefer-strict-equal
+      expect(res.ctx.requestContext).toEqual(e.requestContext);
+      res.end();
+    });
+  });
 });
